588bf966c16538eff1cf24ef98ff2abe83b8dc16
Sam <snapolitano@covermymeds.com>
2022-06-14 19:32:17 +0000

    Add res name

--- network.tf
+++ network.tf
@@ -1,6 +1,11 @@
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "US Central"
+}
+
 module "network" {
   source  = "app.terraform.io/cmm-snapolitano/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "network"
+  resource_group_name = azurerm_resource_group.example.name
 }
